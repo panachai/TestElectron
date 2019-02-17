7493a2b6bd98cc87af67be6439ddbbdcd5e6ce97
Panachai <panachai.ny@gmail.com>
2019-02-17 17:57:17 +0700

    Starter Electron

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-const { app, globalShortcut } = require("electron");
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+const { app, BrowserWindow } = electron;
 
-app.on("ready", () => {
-  // Register a 'CommandOrControl+Y' shortcut listener.
-  globalShortcut.register("CommandOrControl+Y", () => {
-    // Do stuff when Y and either Command/Control is pressed.
+//#region CheckEnv from env.json
+const appEnv = require("./env.json");
+console.log(appEnv);
+if (appEnv.env === "dev") {
+  process.env.NODE_ENV = "development";
+} else if (appEnv.env === "prod") {
+  process.env.NODE_ENV = "production";
+}
+//#endregion
+
+let mainWindow;
+
+app.on("ready", function() {
+  mainWindow = new BrowserWindow({
+    // webPreferences: {
+    //   webSecurity: process.env.NODE_ENV !== "development"
+    // }
   });
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `mainWindows.html`),
+      protocol: `file:`,
+      slashes: true
+    })
+  );
 });
