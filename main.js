2d35897d26e6aa715481a489a9bccd07b93af463
Panachai <panachai.ny@gmail.com>
2019-02-23 18:01:20 +0700

    Add two windows

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 const { app, BrowserWindow, Menu } = electron;
-const { mainMenuTemplete } = require("./templete/mainMenuTemplete");
+// const { mainMenuTemplete } = require("./templete/mainMenuTemplete");
 
 //#region CheckEnv from env.json
 const appEnv = require("./env.json");
@@ -17,6 +17,10 @@ if (appEnv.env === "dev") {
 let mainWindow;
 
 app.on("ready", function() {
+  openMain();
+});
+
+function openMain() {
   mainWindow = new BrowserWindow({
     // webPreferences: {
     //   webSecurity: process.env.NODE_ENV !== "development"
@@ -32,8 +36,53 @@ app.on("ready", function() {
 
   //#region Menu
   //Build Menu from templete
-  const mainMenu = Menu.buildFromTemplate(mainMenuTemplete());
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplete);
   //Insert Menu
   Menu.setApplicationMenu(mainMenu);
   //#endregion Menu
-});
+}
+
+function createAddWindow() {
+  mainWindow = new BrowserWindow({
+    width: 200,
+    height: 300,
+    title: "Add Shopping List Item"
+  });
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `addWindow.html`),
+      protocol: `file:`,
+      slashes: true
+    })
+  );
+
+  //#region Menu
+  //Build Menu from templete
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplete);
+  //Insert Menu
+  Menu.setApplicationMenu(mainMenu);
+  //#endregion Menu
+}
+const mainMenuTemplete = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Add Item",
+        click() {
+          createAddWindow();
+        }
+      },
+      {
+        label: "Clear Item"
+      },
+      {
+        label: "Quit",
+        accelerator: process.platform == "darwin" ? "Command + Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
--- templete/mainMenuTemplete.js
+++ templete/mainMenuTemplete.js
@@ -1,8 +1,27 @@
 module.exports = {
-  mainMenuTemplete: function() {
+  mainMenuTemplete: function(app) {
     return (mainMenuTemplete = [
       {
-        label: "File"
+        label: "File",
+        submenu: [
+          {
+            label: "Add Item",
+            click() {
+              createAddWindow();
+            }
+          },
+          {
+            label: "Clear Item"
+          },
+          {
+            label: "Quit",
+            accelerator:
+              process.platform == "darwin" ? "Command + Q" : "Ctrl+Q",
+            click() {
+              app.quit();
+            }
+          }
+        ]
       }
     ]);
   }
